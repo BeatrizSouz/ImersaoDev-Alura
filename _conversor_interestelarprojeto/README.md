b214d2b7d840e11497c162c2743a5911360cdabb
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2023-02-06 00:20:32 -0300

    atualiza desafio

--- _conversor_interestelarprojeto/README.md
+++ _conversor_interestelarprojeto/README.md
@@ -1,4 +1,5 @@
 # _Conversor_interestelar - Projeto
 
 A Pen created on CodePen.io. Original URL: [https://codepen.io/BeatrizSouz/pen/wvxEBPd](https://codepen.io/BeatrizSouz/pen/wvxEBPd).
+Um conversor converte a distância em anos-luz entre a Terra e a estrela Barnard, em quilômetros.
 
--- _conversor_interestelarprojeto/dist/script.js
+++ _conversor_interestelarprojeto/dist/script.js
@@ -4,9 +4,9 @@ var anoLuzEmKm = 9.4605284;
 var valorKm = anoLuzEmKm * 10 ** 15 * anoLuzEstrela;
 
 alert(
-  "A distância em metros  entre a Terra e a estrela Barnard de " +
+  "A distância em quilômetros  entre a Terra e a estrela Barnard de " +
     anoLuzEstrela +
     " ano-luz é de " +
     valorKm +
-    " m"
+    " Km"
 );
